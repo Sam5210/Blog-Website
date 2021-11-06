683de14e180b80263a7923226d5c78746ff767d9
Samuel <samuelyang5210@gmail.com>
2021-11-07 00:37:04 +0800

    Trying to fix wave animations - failed

--- public/Effects.js
+++ public/Effects.js
@@ -1,6 +1,70 @@
+/*jshint esversion: 6 */
 $(handleEffects);
 function handleEffects(){
     handleCircleEffects();
+}
+function handleTimelineEffects(){
+    let particleDiameter = 0.7;
+    let waveMotionDelayPerParticle = "0.5s";
+    let particleCount = 5;
+    let particleSpaceSize = 2; 
+    let waveInitialPosition = particleCount * (particleSpaceSize + particleDiameter);
+    let wavePositionOffsetY = "-6rem";
+    let wavePositionOffsetX = "1rem";
+    let amplitude = "3rem";
+    let step = 10;
+    let waves = $(".waves");
+    let supportedFlag = $.keyframe.isSupported();
+    if(supportedFlag){
+        $.keyframe.define([{
+            name: 'wave-motion-y',
+            '0%':   {'transform': `translateY(1rem)`},
+            '10%':  {'transform': `translateY(${step})`},
+            '30%':  {'transform': `translateY(${String(2*step)}rem)`},
+            '50%':  {'transform': `translateY(${String(3*step)}rem)`},
+            '70%':  {'transform': `translateY(${String(4*step)}rem)`},
+            '90%':  {'transform': `translateY(${String(5*step)}rem)`},
+            '100%': {'transform': `translateY(${String(6*step)}rem)`}
+        },{
+            name: 'wave-motion-x',
+            '0%':   {'transform': `translateX(1rem)`},
+            '10%':  {'transform': `translateX(-${amplitude});`},
+            '30%':  {'transform': `translateX(${amplitude});`},
+            '50%':  {'transform': `translateX(-${amplitude});`},
+            '70%':  {'transform': `translateX(${amplitude});`},
+            '90%':  {'transform': `translateX(-${amplitude});`},
+            '100%': {'transform': `translateX(0rem)`}
+        }]);
+        let waveUnit = "<div class='wave-motion-x'><div class='wave-particle'></div></div>";
+        let waveUnits = "";
+        for(let index = 1; index <= particleCount; index++){
+            waveUnits += waveUnit;
+        }
+        waves.html(waveUnits);
+        let waveMotionXs = $(".wave-motion-x");
+        let waveParticles = $(".wave-particle");
+        waveMotionXs.each(function(index){
+            $(this).playKeyframe({
+                name: 'wave-motion-x',
+                duration: '10s',
+                iterationCount: 'infinite',
+                direction: 'alternate',
+                timingFunction: 'ease-in-out',
+                delay: waveMotionDelayPerParticle * index
+            });
+        });
+        waveParticles.each(function(index){
+            $(this).playKeyframe({
+                name: 'wave-motion-y',
+                duration: '10s',
+                iterationCount: 'infinite',
+                direction: 'alternate',
+                timingFunction: 'linear',
+                delay: waveMotionDelayPerParticle * index
+            });
+            $(this).css('top', '$wave-position-offsetY + $wave-particle-position');
+        });
+    }
 }
 function handleCircleEffects(){
     let leftCircles = $(".align-hor-left .circle");
@@ -44,7 +108,7 @@ function handleCircleEffects(){
                                     ${offsetBig} ${offsetMedium} 0 2px ${circleBackgroundColor},\
                                     0 0 ${blurSize} ${spreadSize} ${color}`;
 
-    var supportedFlag = $.keyframe.isSupported();
+    let supportedFlag = $.keyframe.isSupported();
     $.keyframe.define([{
         name: 'border',
         '0%': {'box-shadow': borderAnimation0Percent},
@@ -70,7 +134,4 @@ function handleCircleEffects(){
         iterationCount: 1,
         fillMode: 'forwards'
     });
-}
-function handleTimelineEffects(){
-    
 }
