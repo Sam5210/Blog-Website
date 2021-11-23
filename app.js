e89630c80f49839dc8325887639b72d776a7e02f
Samuel <samuelyang5210@gmail.com>
2021-11-23 23:23:15 +0800

    Adding database

--- app.js
+++ app.js
@@ -1,37 +1,80 @@
+//Require the npm modules
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-let entries = [];
-let index = 0;
+//Connect Mongoose
+mongoose.connect("mongodb+srv://JBHSS:Orient2020!@cluster0.zcndj.mongodb.net/Blog", {useNewUrlParser: true});
+
+//Entry schema setup
+const entrySchema = new mongoose.Schema ({
+    title: String,
+    content: String
+});
+const Entry = mongoose.model("Entry", entrySchema);
+
+//Set up app, use ejs, upload files into public through express,
+//add keyframe, set up body parser
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use('/scripts', express.static(__dirname + '/node_modules/jquerykeyframes/dist'));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//Declare variables for the blog
+const defaultEntry = new Entry ({title: "Test", content: "This is a test."});
+const defaultEntries = [defaultEntry, defaultEntry, defaultEntry];
+
+//App posts and gets
+
 app.post("/entry", function(request, response){
-    console.log(request.body);
-    index = Number(request.body.button)
-    response.render("website", {page: "entry", journalEntries: entries, index:index});
+    Entry.findOne({_id: request.body._id}, function(error, entry){
+        if(error){
+            console.log(error);
+        }
+        else{
+            response.render("website", {page: "entry", entry: entry});
+        }
+    });
 })
 app.get("/compose", function(request, response){
-    response.render("website", {page:"compose", journalEntries: entries});
+    response.render("website", {page:"compose"});
 });
 app.post("/", function(request, response){
-    let entry = {
-        title: request.body.title,
-        content: request.body.content
+    let entry = new Entry ({
+        title: request.body.title, 
+        content: request.body.content.replace(/\r\n|\r|\n/g,"<br/>")
+    });
+    let save = async function(){
+        await entry.save();
+        response.redirect("/");
     }
-    console.log("before: ", entry.content);
-    entry.content = entry.content.replace(/\r\n|\r|\n/g,"<br/>");
-    entries.push(entry);
-    console.log("after: ", entry);
-    response.redirect("/");
+    save();
 });
 app.get("/", function(request, response){
     let page = "home";
     if(Object.keys(request.query).length !== 0){
         page = request.query.page;
     }
-    response.render("website", {page:page, journalEntries: entries});
+    Entry.find({}, function(error, entries){
+        if(error){
+            console.log(error);
+        }
+        else{
+            if(entries.length == 0){
+                response.render("website", {page:page, journalEntries: defaultEntries});
+            }
+            else{
+                response.render("website", {page:page, journalEntries: entries});
+            }
+
+        }
+    })
 });
-app.listen(3000);
+
+//App listen
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 3000;
+}
+app.listen(port);
